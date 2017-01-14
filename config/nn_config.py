199fd2ea955b6b428d254298b095fb2401d0b83b
Laura <laura92cp@hotmail.com>
2017-01-14 20:03:28 +0100

    Add files via upload

--- config/nn_config.py
+++ config/nn_config.py
@@ -4,25 +4,24 @@ import numpy as np
 
 def get_parameters():
     nn_params = {}
-    nn_params['mode_in'] = ['mixture3']   # simple, mixture1, mixture2, mixture3
+    nn_params['mode_in'] = 'simple'
     nn_params['nbits'] = 8
     nn_params['framerate'] = 16e3
-    nn_params['nfrec'] = 1
-    nn_params['frequencies'] = [100, 165, 111]
-    #nn_params['frequencies'] = np.random.uniform(1, nn_params['framerate']/24, nn_params['nfrec'])
-    nn_params['sec'] = 5
+    nn_params['nwaves'] = 20
+    nn_params['frequencies'] = np.random.uniform(1, nn_params['framerate']/24, nn_params['nwaves'])
+    nn_params['phases'] = [0, np.pi/2, np.pi]
+    nn_params['sec'] = 1
 
-    nn_params['nb_filters'] = 32
+    nn_params['nb_filters'] = 48
     nn_params['batch_size'] = 256
-    nn_params['nbatch_per_epoch'] = 12
+    nn_params['batches_per_epoch'] = 10
     nn_params['nb_epoch'] = 80
-    nn_params['permutations'] = nn_params['batch_size'] * nn_params['nbatch_per_epoch'] * nn_params['nb_epoch']
+    #nn_params['permutations'] = nn_params['batch_size'] * nn_params['nbatch_per_epoch'] * nn_params['nb_epoch']
     nn_params['load_weight'] = 'False'
     nn_params['save_weight'] = 'True'
-    nn_params['wfilepath'] = './weights/weights-simple-100e-256batch.hdf5'
+    nn_params['wfilepath'] = './weights/weights-simple-80e-256batch.hdf5'
 
-    nn_params['synthesize'] = 'False'
-    nn_params['generation-length'] = 2*(nn_params['framerate']-1)
-    nn_params['sampling_frequency'] = 32000
+    nn_params['synthesize'] = 'True'
+    nn_params['samples'] = 50
 
     return nn_params
