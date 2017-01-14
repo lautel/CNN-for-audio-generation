91b1c9ff153c40f5f9acfbab2d60105587b481ca
Laura <laura92cp@hotmail.com>
2017-01-14 20:05:29 +0100

    Add files via upload

--- gen_utils/sequence_generator.py
+++ gen_utils/sequence_generator.py
@@ -1,11 +1,13 @@
 import numpy as np
+from IPython import embed
+import matplotlib.pyplot as plt
 
 # Extrapolates from a given seed sequence
-# Code based on this source: https://github.com/MattVitelli/GRUV/blob/master/gen_utils/sequence_generator.py
+# Source: https://github.com/MattVitelli/GRUV/blob/master/gen_utils/sequence_generator.py
 
 
 def generate_from_seed(model, seed, sequence_length, data_variance, data_mean, values_):
-    seed = np.reshape(seed, (1,seed.shape[0], seed.shape[1]))
+    #seed = np.reshape(seed, (1,seed.shape[0], seed.shape[1]))
     seedSeq = seed.copy()
     output=[]
     seedNum = values_[np.argmax(seedSeq[0], axis=1)]
@@ -28,6 +30,22 @@ def generate_from_seed(model, seed, sequence_length, data_variance, data_mean, v
         newSeq = np.reshape(seedSeqNew, (1, 1, seedSeqNew.shape[1]))
         seedSeq = np.concatenate((seedSeq, newSeq), axis=1)
         seedSeq = np.delete(seedSeq, 0, axis=1)
+
+
+    # for it in xrange(sequence_length):
+    #     prediction = model.predict_classes(seedSeq)  # Step 1. Generate sample X_n + 1
+    #     out[0,it+seedNum.shape[1]] = values_[prediction]
+    #     seedSeqNew = np.zeros((1,seedSeq.shape[2]))
+    #     seedSeqNew[0, prediction] = 1
+    #     # one-hot encoding for making the prediction again
+    #
+    #     if it == 0:
+    #         output.append(np.concatenate((seedSeq[0], seedSeqNew.copy()), axis=0))
+    #     else:
+    #         output.append(seedSeqNew[0][0:seedSeqNew.shape[1]].copy())
+    #     newSeq = np.reshape(seedSeqNew, (1, 1, seedSeqNew.shape[1]))
+    #     seedSeq = np.concatenate((seedSeq, newSeq), axis=1)
+    #     seedSeq = np.delete(seedSeq, 0, axis=1)
 
     # Finally, post-process the generated sequence so that we have valid frequencies
     # We're essentially just undo-ing the data centering process
