30b0712c7264f0a622a77d8231743cbacff82646
Laura <laura92cp@hotmail.com>
2017-01-23 16:36:20 +0100

    Update nn_config.py

--- config/nn_config.py
+++ config/nn_config.py
@@ -6,22 +6,25 @@ def get_parameters():
     nn_params = {}
     nn_params['mode_in'] = 'simple'
     nn_params['nbits'] = 8
-    nn_params['framerate'] = 16e3
-    nn_params['nwaves'] = 20
+    nn_params['framerate'] = 8e3  ##
+    nn_params['nwaves'] = 1000
     nn_params['frequencies'] = np.random.uniform(1, nn_params['framerate']/24, nn_params['nwaves'])
     nn_params['phases'] = [0, np.pi/2, np.pi]
     nn_params['sec'] = 1
 
-    nn_params['nb_filters'] = 32
-    nn_params['batch_size'] = 256
+    nn_params['receptive_field'] = 1024
+    nn_params['stride'] = 1  #
+    #nn_params['window_length'] = nn_params['stride'] + nn_params['receptive_field']
+    nn_params['nb_filters'] = 256
+    nn_params['batch_size'] = 32
     nn_params['batches_per_epoch'] = 10
-    nn_params['nb_epoch'] = 80
+    nn_params['nb_epoch'] = 150
     #nn_params['permutations'] = nn_params['batch_size'] * nn_params['nbatch_per_epoch'] * nn_params['nb_epoch']
     nn_params['load_weight'] = 'False'
     nn_params['save_weight'] = 'True'
-    nn_params['wfilepath'] = './weights/weights-simple-80e-256batch.hdf5'
+    nn_params['wfilepath'] = './weights/weights-segments_3.hdf5'
 
-    nn_params['synthesize'] = 'True'
+    nn_params['synthesize'] = 'False'
     nn_params['samples'] = 50
 
     return nn_params
