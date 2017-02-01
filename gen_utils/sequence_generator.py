f303534f3a1c365101b1f5fe12f21a150959137a
Laura <laura92cp@hotmail.com>
2017-02-01 14:53:23 +0100

    Update sequence_generator.py

--- gen_utils/sequence_generator.py
+++ gen_utils/sequence_generator.py
@@ -1,33 +1,30 @@
 import numpy as np
+from tqdm import tqdm
 from IPython import embed
-import matplotlib.pyplot as plt
+from data_utils.functions import one_hot_encoding
 
-# Based on this resource: https://github.com/MattVitelli/GRUV/blob/master/gen_utils/sequence_generator.py
+# Extrapolates from a given seed sequence
+# Source: https://github.com/MattVitelli/GRUV/blob/master/gen_utils/sequence_generator.py
 
 
 def generate_from_seed(model, seed, sequence_length, data_variance, data_mean, values_):
     #seed = np.reshape(seed, (1,seed.shape[0], seed.shape[1]))
     seedSeq = seed.copy()
-    output=[]
-    seedNum = values_[np.argmax(seedSeq[0], axis=1)]
-    seedNum = np.reshape(seedNum, (1, len(seedNum)))
-    out = np.concatenate((seedNum, np.zeros((1,sequence_length))), axis=1)
 
-    for it in xrange(sequence_length):
-        prediction = model.predict(seedSeq)  # Step 1. Generate sample X_n + 1
-        predict_class = np.argmax(prediction, axis=1)
-        out[0,it+seedNum.shape[1]] = values_[predict_class]
+    #seedNum = values_[np.argmax(seedSeq[0])]
+    #seedNum = np.reshape(seedNum, (1, len(seedNum)))
+    out = np.concatenate((seedSeq, np.zeros((1,sequence_length))), axis=1)
+    #out=np.zeros((1,sequence_length))
+    for it in tqdm(xrange(sequence_length)):
+        seedSeq = out[:, it : it+seedSeq.shape[1]]
+        oh_seed = one_hot_encoding(seedSeq, values_)
+        prediction = model.predict(oh_seed, batch_size=32)  # Step 1. Generate sample X_n + 1
+        predict_class = np.argmax(prediction[0], axis=1)
+        out[0,it+seedSeq.shape[1]] = values_[predict_class[-1]]
 
-        seedSeqNew = np.zeros((1,seedSeq.shape[2]))
-        seedSeqNew[0, predict_class] = 1
-        # one-hot encoding for making the prediction again
-
-        if it == 0:
-            output.append(np.concatenate((seedSeq[0], seedSeqNew.copy()), axis=0))
-        else:
-            output.append(seedSeqNew[0][0:seedSeqNew.shape[1]].copy())
-        newSeq = np.reshape(seedSeqNew, (1, 1, seedSeqNew.shape[1]))
-        seedSeq = np.concatenate((seedSeq, newSeq), axis=1)
-        seedSeq = np.delete(seedSeq, 0, axis=1)
-
-    return out, output
+    # Finally, post-process the generated sequence so that we have valid frequencies
+    # We're essentially just undo-ing the data centering process
+    # for i in xrange(len(output)):
+    #     output[i] *= data_variance
+    #     output[i] += data_mean
+    return out
